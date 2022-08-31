0e57088bacc90c9e23cc20376d6719ba604e1aa9
LucHauser <90209377+LucHauser@users.noreply.github.com>
2022-08-31 09:23:42 +0200

    Added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('has placeholder', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByPlaceholderText(/please input name/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('has elment with class formAdd', () => {
+  const {container} = render(<App />);
+
+  const boxes = container.getElementsByClassName('formAdd');
+
+  console.log(boxes.length); // 👉️ 2
+
+  expect(boxes.length).toBe(1);
 });
